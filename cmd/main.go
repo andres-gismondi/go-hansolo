c455f5f60fb63ae80164950d101bfcefdc27eb22
Andres Julian Gismondi <andrees2488@gmail.com>
2020-11-28 19:29:16 -0300

    add http

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
-	"fmt"
-	"go-hansolo/pkg/server/services"
+	"go-hansolo/internal/server"
+	"log"
+	"os"
+	"os/signal"
 )
 
 func main() {
 	//log.Fatal(http.ListenAndServe(":8080", nil))
-	resultX, resultY := services.GetLocation(500, 200, 1538)
+	/*resultX, resultY := services.GetLocation(500, 200, 1538)
 	fmt.Println(resultX, resultY)
 	m1 := []string{"", "es","","mensaje"}
 	m2 := []string{"", "","un",""}
 	m3 := []string{"este", "es","",""}
 	message := services.GetMessage(m1, m2, m3)
-	fmt.Println(message)
+	fmt.Println(message)*/
+
+	port := os.Getenv("PORT")
+	serv, err := server.New(port)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// start the server.
+	go serv.Start()
+
+	// Wait for an in interrupt.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	// Attempt a graceful shutdown.
+	_ = serv.Close()
 }
--- pkg/server/controller/api.go
+++ pkg/server/controller/api.go
@@ -1,1 +1,23 @@
 package server
+
+import (
+	"github.com/go-chi/chi"
+	"go-hansolo/pkg/server/services"
+	"net/http"
+)
+
+func New() http.Handler {
+	r := chi.NewRouter()
+
+	r.Get("/", services.GetLocation(500, 200, 1538))
+
+	/*r.Post("/", ur.CreateHandler)
+
+	r.Get("/{id}", ur.GetOneHandler)
+
+	r.Put("/{id}", ur.UpdateHandler)
+
+	r.Delete("/{id}", ur.DeleteHandler)*/
+
+	return r
+}
