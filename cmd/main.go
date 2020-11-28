bcf70f5dcea3acaacecb49eff9ec5f008eddd615
Andres Julian Gismondi <andrees2488@gmail.com>
2020-11-28 13:44:44 -0300

    add getmessage and intersection point functionality

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"fmt"
+	"go-hansolo/pkg/server/services"
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	//log.Fatal(http.ListenAndServe(":8080", nil))
+	resultX, resultY := services.GetLocation(500, 200, 1538)
+	fmt.Println(resultX, resultY)
+	m1 := []string{"", "es","","mensaje"}
+	m2 := []string{"", "","un",""}
+	m3 := []string{"este", "es","",""}
+	message := services.GetMessage(m1, m2, m3)
+	fmt.Println(message)
 }
